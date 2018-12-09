070653f04d3a4716ce7eca927dcec8ece11bf658
Dean Chalk <dean@deanchalk.com>
2018-12-09 11:53:38 +0000

    inital commit

---
+++ DispatcherSafeObservableCollectionExample/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+
+namespace DispatcherSafeObservableCollectionExample
+{
+    public partial class MainWindow : Window
+    {
+        private readonly SafeObservable<TestData> data =
+            new SafeObservable<TestData>();
+
+        private readonly Random rand = new Random(DateTime.Now.Millisecond);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            list.DataContext = data;
+            var work = new List<Action>();
+            for (var i = 0; i < 100; i++)
+            {
+                work.Add(DoWorkAdd);
+                work.Add(DoWorkClear);
+                work.Add(DoWorkRemove);
+                work.Add(DoWorkRemoveAt);
+                work.Add(DoWorkInsert);
+                work.Add(DoWorkReplace);
+            }
+
+            for (var i = 0; i < 1000; i++)
+                work[rand.Next(0, work.Count)]
+                    .BeginInvoke(null, null);
+        }
+
+        private void DoWorkAdd()
+        {
+            Thread.Sleep(rand.Next(500, 30000));
+            data.Add(new TestData
+            {
+                Text = $"Thread {Thread.CurrentThread.ManagedThreadId} Added"
+            });
+        }
+
+        private void DoWorkClear()
+        {
+            Thread.Sleep(rand.Next(500, 10000));
+            data.Clear();
+            Debug.WriteLine("Thread {0} Clear", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private void DoWorkRemove()
+        {
+            Thread.Sleep(rand.Next(500, 10000));
+            if (data.Count == 0)
+                return;
+            var item = data[0];
+            data.Remove(item);
+            Debug.WriteLine("Thread {0} Remove", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private void DoWorkRemoveAt()
+        {
+            Thread.Sleep(rand.Next(500, 10000));
+            if (data.Count == 0)
+                return;
+            data.RemoveAt(0);
+            Debug.WriteLine("Thread {0} RemoveAt", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private void DoWorkInsert()
+        {
+            Thread.Sleep(rand.Next(500, 10000));
+            data.Insert(rand.Next(0, data.Count), new TestData
+            {
+                Text = $"Thread {Thread.CurrentThread.ManagedThreadId} Insert"
+            });
+        }
+
+        private void DoWorkReplace()
+        {
+            Thread.Sleep(rand.Next(500, 10000));
+            data[rand.Next(0, data.Count)] = new TestData
+            {
+                Text = $"Thread {Thread.CurrentThread.ManagedThreadId} Replace"
+            };
+        }
+
+        private class TestData
+        {
+            public string Text { get; set; }
+        }
+    }
+}
---
+++ DispatcherSafeObservableCollectionExample/SafeObservable.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace DispatcherSafeObservableCollectionExample
+{
+    public class SafeObservable<T> : IList<T>, INotifyCollectionChanged
+    {
+        private readonly IList<T> collection = new List<T>();
+        private readonly Dispatcher dispatcher;
+        private readonly ReaderWriterLock sync = new ReaderWriterLock();
+
+        public SafeObservable()
+        {
+            dispatcher = Dispatcher.CurrentDispatcher;
+        }
+
+        public void Add(T item)
+        {
+            if (Thread.CurrentThread == dispatcher.Thread)
+                DoAdd(item);
+            else
+                dispatcher.BeginInvoke((Action) (() => { DoAdd(item); }));
+        }
+
+        public void Clear()
+        {
+            if (Thread.CurrentThread == dispatcher.Thread)
+                DoClear();
+            else
+                dispatcher.BeginInvoke((Action) (DoClear));
+        }
+
+        public bool Contains(T item)
+        {
+            sync.AcquireReaderLock(Timeout.Infinite);
+            var result = collection.Contains(item);
+            sync.ReleaseReaderLock();
+            return result;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            sync.AcquireWriterLock(Timeout.Infinite);
+            collection.CopyTo(array, arrayIndex);
+            sync.ReleaseWriterLock();
+        }
+
+        public int Count
+        {
+            get
+            {
+                sync.AcquireReaderLock(Timeout.Infinite);
+                var result = collection.Count;
+                sync.ReleaseReaderLock();
+                return result;
+            }
+        }
+
+        public bool IsReadOnly => collection.IsReadOnly;
+
+        public bool Remove(T item)
+        {
+            if (Thread.CurrentThread == dispatcher.Thread) return DoRemove(item);
+
+            var op = dispatcher.BeginInvoke(
+                new Func<T, bool>(DoRemove), item);
+            if (op.Result == null)
+                return false;
+            return (bool) op.Result;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return collection.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return collection.GetEnumerator();
+        }
+
+        public int IndexOf(T item)
+        {
+            sync.AcquireReaderLock(Timeout.Infinite);
+            var result = collection.IndexOf(item);
+            sync.ReleaseReaderLock();
+            return result;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (Thread.CurrentThread == dispatcher.Thread)
+                DoInsert(index, item);
+            else
+                dispatcher.BeginInvoke((Action) (() => { DoInsert(index, item); }));
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (Thread.CurrentThread == dispatcher.Thread)
+                DoRemoveAt(index);
+            else
+                dispatcher.BeginInvoke((Action) (() => { DoRemoveAt(index); }));
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                sync.AcquireReaderLock(Timeout.Infinite);
+                var result = collection[index];
+                sync.ReleaseReaderLock();
+                return result;
+            }
+            set
+            {
+                sync.AcquireWriterLock(Timeout.Infinite);
+                if (collection.Count == 0 || collection.Count <= index)
+                {
+                    sync.ReleaseWriterLock();
+                    return;
+                }
+
+                collection[index] = value;
+                sync.ReleaseWriterLock();
+            }
+        }
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        private void DoAdd(T item)
+        {
+            sync.AcquireWriterLock(Timeout.Infinite);
+            collection.Add(item);
+            CollectionChanged?.Invoke(this,
+    new NotifyCollectionChangedEventArgs(
+        NotifyCollectionChangedAction.Add, item));
+            sync.ReleaseWriterLock();
+        }
+
+        private void DoClear()
+        {
+            sync.AcquireWriterLock(Timeout.Infinite);
+            collection.Clear();
+            CollectionChanged?.Invoke(this,
+    new NotifyCollectionChangedEventArgs(
+        NotifyCollectionChangedAction.Reset));
+            sync.ReleaseWriterLock();
+        }
+
+        private bool DoRemove(T item)
+        {
+            sync.AcquireWriterLock(Timeout.Infinite);
+            var index = collection.IndexOf(item);
+            if (index == -1)
+            {
+                sync.ReleaseWriterLock();
+                return false;
+            }
+
+            var result = collection.Remove(item);
+            if (result)
+                CollectionChanged?.Invoke(this, new
+                    NotifyCollectionChangedEventArgs(
+                        NotifyCollectionChangedAction.Reset));
+            sync.ReleaseWriterLock();
+            return result;
+        }
+
+        private void DoInsert(int index, T item)
+        {
+            sync.AcquireWriterLock(Timeout.Infinite);
+            collection.Insert(index, item);
+            CollectionChanged?.Invoke(this,
+                new NotifyCollectionChangedEventArgs(
+                    NotifyCollectionChangedAction.Add, item, index));
+            sync.ReleaseWriterLock();
+        }
+
+        private void DoRemoveAt(int index)
+        {
+            sync.AcquireWriterLock(Timeout.Infinite);
+            if (collection.Count == 0 || collection.Count <= index)
+            {
+                sync.ReleaseWriterLock();
+                return;
+            }
+
+            collection.RemoveAt(index);
+            CollectionChanged?.Invoke(this,
+                new NotifyCollectionChangedEventArgs(
+                    NotifyCollectionChangedAction.Reset));
+            sync.ReleaseWriterLock();
+        }
+    }
+}
